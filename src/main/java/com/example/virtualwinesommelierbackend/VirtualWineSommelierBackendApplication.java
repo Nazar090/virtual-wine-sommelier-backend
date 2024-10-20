59a69535096790e8bf1aac1797824537ede79a3e
Nazar <nazarmo13@gmail.com>
2024-10-20 08:27:40 +0100

    Initial commit on main branch

---
+++ src/main/java/com/example/virtualwinesommelierbackend/VirtualWineSommelierBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualwinesommelierbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWineSommelierBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualWineSommelierBackendApplication.class, args);
+    }
+
+}
