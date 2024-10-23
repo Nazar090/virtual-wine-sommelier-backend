e2687e40fd11e647968911f07ac859801465525a
Nazar <nazarmo13@gmail.com>
2024-10-23 14:13:19 +0100

    add UserService and UserServiceImpl

--- src/main/java/com/example/virtualwinesommelierbackend/service/UserService.java
+++ src/main/java/com/example/virtualwinesommelierbackend/service/UserService.java
@@ -1,2 +1,11 @@
-package com.example.virtualwinesommelierbackend.service;public interface UserService {
+package com.example.virtualwinesommelierbackend.service;
+
+import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationDto;
+import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationRequestDto;
+import com.example.virtualwinesommelierbackend.exception.RegistrationException;
+
+public interface UserService {
+    UserRegistrationDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException;
+
 }
