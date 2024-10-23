6b4b8c9b91a028fd71d6d8ff59a5899f73b4868d
Nazar <nazarmo13@gmail.com>
2024-10-23 14:08:06 +0100

    Add JWT authentication and user details service for security

--- src/main/java/com/example/virtualwinesommelierbackend/security/AuthenticationService.java
+++ src/main/java/com/example/virtualwinesommelierbackend/security/AuthenticationService.java
@@ -1,24 +1,34 @@
-package com.example.onlinebookstore.security;
+package com.example.virtualwinesommelierbackend.security;
 
-import com.example.onlinebookstore.dto.user.UserLoginRequestDto;
-import com.example.onlinebookstore.dto.user.UserLoginResponseDto;
+import com.example.virtualwinesommelierbackend.dto.user.UserLoginDto;
+import com.example.virtualwinesommelierbackend.dto.user.UserLoginRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service responsible for authentication users and generating JWT tokens.
+ */
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
 
-    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+    /**
+     * Authenticates the user and generates a JWT token upon successful authentication.
+     * @param requestDto Login request for the user to log in to the account.
+     * @return Generated JWT token for the authenticated user
+     */
+    public UserLoginDto authenticate(UserLoginRequestDto requestDto) {
         final Authentication authentication = authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(requestDto.email(), requestDto.password())
         );
         String token = jwtUtil.generateToken(authentication.getName());
-        return new UserLoginResponseDto(token);
+
+        // Return the token wrapped in UserLoginDto
+        return new UserLoginDto(token);
     }
 }
--- src/main/java/com/example/virtualwinesommelierbackend/security/CustomUserDetailsService.java
+++ src/main/java/com/example/virtualwinesommelierbackend/security/CustomUserDetailsService.java
@@ -1,18 +1,31 @@
-package com.example.onlinebookstore.security;
+package com.example.virtualwinesommelierbackend.security;
 
-import com.example.onlinebookstore.exception.EntityNotFoundException;
-import com.example.onlinebookstore.repository.UserRepository;
+import com.example.virtualwinesommelierbackend.exception.EntityNotFoundException;
+import com.example.virtualwinesommelierbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * Custom implementation of {@link UserDetailsService} that loads user-specific data
+ * during authentication
+ * This service interacts with {@link UserRepository} to fetch user data from the storage.
+ */
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
+    /**
+     * Locates the user based on the email provided.
+     *
+     * @param email The user's login email address
+     * @return The {@link UserDetails} object containing user information
+     * @throws UsernameNotFoundException if the user with a given email cannot be found
+     * @throws EntityNotFoundException if no user with the given email exists
+     */
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
