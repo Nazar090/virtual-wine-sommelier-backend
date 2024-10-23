f77e7859738904d7bb86b9e5c77d56ddf4f2b969
Nazar <nazarmo13@gmail.com>
2024-10-23 14:09:59 +0100

    Add repositories for User, Role, and Address entities

--- src/main/java/com/example/virtualwinesommelierbackend/repository/AddressRepository.java
+++ src/main/java/com/example/virtualwinesommelierbackend/repository/AddressRepository.java
@@ -1,2 +1,8 @@
-package com.example.virtualwinesommelierbackend.repository;public interface AddressRepository {
+package com.example.virtualwinesommelierbackend.repository;
+
+import com.example.virtualwinesommelierbackend.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
