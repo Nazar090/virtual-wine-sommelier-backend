7ff3815b4167afca15dc5ea7176113bcaed6978b
Nazar <nazarmo13@gmail.com>
2024-10-23 14:10:19 +0100

    Add mappers for converting between DTOs and entity models

--- src/main/java/com/example/virtualwinesommelierbackend/mapper/AddressMapper.java
+++ src/main/java/com/example/virtualwinesommelierbackend/mapper/AddressMapper.java
@@ -4,7 +4,6 @@ import com.example.virtualwinesommelierbackend.config.MapperConfig;
 import com.example.virtualwinesommelierbackend.dto.address.AddressRequestDto;
 import com.example.virtualwinesommelierbackend.model.Address;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface AddressMapper {
