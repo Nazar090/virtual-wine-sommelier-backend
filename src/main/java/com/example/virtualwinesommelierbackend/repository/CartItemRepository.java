73144b287dbeccfff0deff1c711220a6d64ac804
Nazar <nazarmo13@gmail.com>
2024-11-05 11:10:43 +0000

    added functional description

--- src/main/java/com/example/virtualwinesommelierbackend/repository/CartItemRepository.java
+++ src/main/java/com/example/virtualwinesommelierbackend/repository/CartItemRepository.java
@@ -8,8 +8,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    /**
+     * Finds a CartItem by its ID and the associated ShoppingCart ID.
+     *
+     * @param cartItemId the ID of the CartItem
+     * @param shoppingCartId the ID of the associated ShoppingCart
+     * @return an Optional containing the CartItem if found, or empty if not found
+     */
     Optional<CartItem> findByIdAndShoppingCartId(Long cartItemId, Long shoppingCartId);
 
+    /**
+     * Deletes all CartItems associated with a specific ShoppingCart ID.
+     *
+     * @param cartId the ID of the ShoppingCart whose items should be deleted
+     */
     @Modifying
     @Query("DELETE FROM CartItem ci WHERE ci.shoppingCart.id = :cartId")
     void deleteAllByShoppingCartId(@Param("cartId") Long cartId);
--- src/main/java/com/example/virtualwinesommelierbackend/repository/OrderRepository.java
+++ src/main/java/com/example/virtualwinesommelierbackend/repository/OrderRepository.java
@@ -4,5 +4,4 @@ import com.example.virtualwinesommelierbackend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
 }
