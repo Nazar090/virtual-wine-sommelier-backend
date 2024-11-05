c09fbf4a235832d9cd40156604e2cb0590bc17b9
Nazar <nazarmo13@gmail.com>
2024-11-05 10:47:02 +0000

    added&corrected repositories and mappers

---
+++ src/main/java/com/example/virtualwinesommelierbackend/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.virtualwinesommelierbackend.repository;
+
+import com.example.virtualwinesommelierbackend.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
