39b1525d1cd3d24a6590773dbb3dc330f28e5632
Nazar <nazarmo13@gmail.com>
2024-11-11 15:00:37 +0000

    configured docker and aws in my project

--- src/main/java/com/example/virtualwinesommelierbackend/VirtualWineSommelierBackendApplication.java
+++ src/main/java/com/example/virtualwinesommelierbackend/VirtualWineSommelierBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.virtualwinesommelierbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualWineSommelierBackendApplication {
 
     public static void main(String[] args) {
+        // Load environment variables from the .env file
+        Dotenv dotenv = Dotenv.configure().load();
+
         SpringApplication.run(VirtualWineSommelierBackendApplication.class, args);
     }
 
