92d9cad5ce6843e9d3426deab65c803c9d83a337
Nazar <nazarmo13@gmail.com>
2024-11-12 10:05:38 +0000

    added DTO and mapper

--- src/main/java/com/example/virtualwinesommelierbackend/mapper/UserMapper.java
+++ src/main/java/com/example/virtualwinesommelierbackend/mapper/UserMapper.java
@@ -3,14 +3,21 @@ package com.example.virtualwinesommelierbackend.mapper;
 import com.example.virtualwinesommelierbackend.config.MapperConfig;
 import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationDto;
 import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationRequestDto;
+import com.example.virtualwinesommelierbackend.dto.user.profile.UserDto;
+import com.example.virtualwinesommelierbackend.dto.user.profile.UserRequestDto;
 import com.example.virtualwinesommelierbackend.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class, uses = AddressMapper.class)
 public interface UserMapper {
-    UserRegistrationDto toDto(User user);
+    UserRegistrationDto toRegistrationDto(User user);
+
+    UserDto toDto(User user);
 
     @Mapping(target = "shippingAddress", source = "shippingAddress")
-    User toUserEntity(UserRegistrationRequestDto requestDto);
+    User toUserEntityFromRegistration(UserRegistrationRequestDto requestDto);
+
+    void updateUserInfo(UserRequestDto requestDto, @MappingTarget User user);
 }
