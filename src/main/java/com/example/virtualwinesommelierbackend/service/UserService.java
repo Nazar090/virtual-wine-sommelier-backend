4d2d6e422d21b1109af76f879e00e86a94c4b0c3
Nazar <nazarmo13@gmail.com>
2024-11-12 10:06:12 +0000

    realized logic in services

--- src/main/java/com/example/virtualwinesommelierbackend/service/UserService.java
+++ src/main/java/com/example/virtualwinesommelierbackend/service/UserService.java
@@ -3,11 +3,14 @@ package com.example.virtualwinesommelierbackend.service;
 import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationDto;
 import com.example.virtualwinesommelierbackend.dto.user.UserRegistrationRequestDto;
 import com.example.virtualwinesommelierbackend.dto.user.profile.UserDto;
+import com.example.virtualwinesommelierbackend.dto.user.profile.UserRequestDto;
 import com.example.virtualwinesommelierbackend.exception.RegistrationException;
 
 public interface UserService {
     UserRegistrationDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException;
 
-    UserDto getUserInfo();
+    UserDto getUserInfo(Long userId);
+
+    UserDto updateUserInfo(UserRequestDto requestDto);
 }
