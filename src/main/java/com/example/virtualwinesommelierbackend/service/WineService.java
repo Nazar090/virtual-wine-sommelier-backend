e46c1eb97f3e15c84e6a0d8e044cd7286bc8c059
Nazar <nazarmo13@gmail.com>
2024-12-18 19:03:00 +0000

    changed data output from /api/products endpoint

--- src/main/java/com/example/virtualwinesommelierbackend/service/WineService.java
+++ src/main/java/com/example/virtualwinesommelierbackend/service/WineService.java
@@ -1,16 +1,15 @@
 package com.example.virtualwinesommelierbackend.service;
 
+import com.example.virtualwinesommelierbackend.dto.wine.ProductDto;
 import com.example.virtualwinesommelierbackend.dto.wine.WineDto;
 import com.example.virtualwinesommelierbackend.dto.wine.WineRequestDto;
-import java.util.List;
-import org.springframework.data.domain.Pageable;
 
 public interface WineService {
     WineDto save(WineRequestDto requestDto);
 
     WineDto getById(Long id);
 
-    List<WineDto> getAll(Pageable pageable);
+    ProductDto getAll();
 
     WineDto update(Long id, WineRequestDto requestDto);
 
