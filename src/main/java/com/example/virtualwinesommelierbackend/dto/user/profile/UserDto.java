82e7589c5818521e3ed4fbd32559514e37bfcd75
Nazar <nazarmo13@gmail.com>
2024-12-22 15:40:32 +0000

    fixed issues in DTOs and others

--- src/main/java/com/example/virtualwinesommelierbackend/dto/user/profile/UserDto.java
+++ src/main/java/com/example/virtualwinesommelierbackend/dto/user/profile/UserDto.java
@@ -1,12 +1,14 @@
 package com.example.virtualwinesommelierbackend.dto.user.profile;
 
 import com.example.virtualwinesommelierbackend.dto.address.AddressDto;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Getter
 @Setter
+@Data
 @Accessors(chain = true)
 public class UserDto {
     private Long id;
